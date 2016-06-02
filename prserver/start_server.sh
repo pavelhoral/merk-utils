f3aa5e513113ca181d6623da06e84b28fe5ba9fb
Pavel Horal <pavel.horal@orchitech.cz>
2016-06-02 12:34:56 +0200

    SERVER_NAME introduced and exit code is now logged.

--- prserver/start_server.sh
+++ prserver/start_server.sh
@@ -5,12 +5,17 @@
 # Supported variables:
 #  
 # - SERVER_BASE = Base directory for the server (defaults to script's dirname).
+# - SERVER_NAME = Symbolic name of the server (defaults to basename of `$SERVER_BASE`).
 # - BACKUP_BASE = Base directory for data backup (defaults to `$SERVER_BASE/backup`).
 # - TCPDUMP_BASE = Base directory for PCAP files (defaults to `$SERVER_BASE/netdump`).
 # - LOG_FILE = Path to the script's log file (defaults to `$SERVER_BASE/server.log`).
 
 if [ -z "$SERVER_BASE" ]; then
    SERVER_BASE=$(dirname "$0")
+fi
+if [ -z "$SERVER_NAME" ]; then
+   SERVER_BASE=$(realpath "$SERVER_BASE")
+   SERVER_NAME=$(basename "$SERVER_BASE")
 fi
 if [ -z "$BACKUP_BASE" ]; then
     BACKUP_BASE="$SERVER_BASE/backup"
@@ -24,7 +29,7 @@ fi
 
 # Write message to a log file
 log_message() {
-    echo "$(date +"%Y-%m-%d %H:%M:%S") $1" >> $LOG_FILE
+    echo "$(date +"%Y-%m-%d %H:%M:%S") [$SERVER_NAME] $1" >> $LOG_FILE
 }
 
 # Start the server
@@ -35,7 +40,7 @@ start_server() {
 
 # Collect debug information
 collect_debug() {
-    local PREFIX=$(date +"%Y-%m-%d_%H:%M:%S")
+    local PREFIX="$(date +"%Y-%m-%d_%H:%M:%S")_$SERVER_NAME"
     mv core "$BACKUP_BASE/$PREFIX"_core
     cp "$TCPDUMP_BASE"/$(ls -1t "$TCPDUMP_BASE" | head -1) "$BACKUP_BASE/$PREFIX"_tcpdump
 }
@@ -50,11 +55,12 @@ trap 'log_message "Server execution interrupted... aborting restart cycle."; exi
 RESTART_COUNTER=0
 while true; do
     start_server
-    if [ $? -eq 139 ]; then
+    EXIT_CODE=$?
+    if [ $EXIT_CODE -eq 139 ]; then
         collect_debug
     fi
     RESTART_COUNTER=$((RESTART_COUNTER+=1))
-    log_message "Server stopped and will be restarted [COUNTER=$RESTART_COUNTER]."
+    log_message "Server stopped [CODE=$EXIT_CODE] and will be restarted [COUNTER=$RESTART_COUNTER]."
     sleep 10
 done
 
