434a311074c2b3eac577caa145371e6935ffcf15
root <root@merksrv.merkpr.co.uk>
2016-06-13 15:00:07 +0200

    Added change_startmap.sh and prservers service

---
+++ prserver/change_startmap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+maps[0]='mapList.append jabal gpm_skirmish 16'
+maps[1]='mapList.append qwai1 gpm_skirmish 16'
+maps[2]='mapList.append sbeneh_outskirts gpm_skirmish 16'
+maps[3]='mapList.append hill_488 gpm_skirmish 16'
+maps[4]='mapList.append nuijamaa gpm_skirmish 16'
+maps[5]='mapList.append lashkar_valley gpm_skirmish 16'
+maps[6]='mapList.append kokan gpm_skirmish 16'
+maps[7]='mapList.append shijiavalley gpm_skirmish 16'
+maps[8]='mapList.append bijar_canyons gpm_skirmish 16'
+maps[9]='mapList.append burning_sands gpm_skirmish 16'
+
+rand=$[$RANDOM % 10]
+echo $(date)
+echo ${maps[$rand]}
+theMap=${maps[$rand]}
+
+#Create temporary file with new line in place
+cat /home/pr/prMain/mods/pr/settings/maplist.con | sed -e "19s/.*/${theMap}/" > /tmp/temp_maplist.con
+
+#Copy the new file over the original file
+mv /tmp/temp_maplist.con /home/pr/prMain/mods/pr/settings/maplist.con
