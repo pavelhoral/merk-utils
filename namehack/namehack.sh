bbea5793319476fa20f29ee879ef07f24345c022
Pavel Horal <pavel.horal@orchitech.cz>
2016-07-23 14:06:32 +0200

    Namehack name rule append logic.

--- namehack/namehack.sh
+++ namehack/namehack.sh
@@ -6,6 +6,7 @@
 #  
 # - SERVER_PORT = Server port (defaults to 16567).
 # - CHECK_CHAIN = Name of the checking IPTABLES chain (defaults to NAMEHACK).
+# - CHECK_PREFIX = Log prefix for checking chain (defaults to "$CHECK_CHAIN: ").
 # - CHECK_FILTER = Rule for u32 netfilter (not intended to be modified).
 # - REJECT_CHAIN = Name of the rejecting IPTABLES chain (defaults to "$CHECK_CHAIN"_REJECT).
 # - REJECT_PREFIX = Log prefix for rejection chain (defaults to "$REJECT_CHAIN: ").
@@ -18,6 +19,7 @@ fi
 if [ -z "$CHECK_CHAIN" ]; then
     CHECK_CHAIN="NAMEHACK"
 fi
+CHECK_PREFIX="$CHECK_CHAIN: "
 CHECK_FILTER="0>>22&0x3C@8>>24&0xEF=0x0F && 0>>22&0x3C@20=0x01000000 && 0>>22&0x3C@25=0x00000004"
 
 if [ -z "$REJECT_CHAIN" ]; then
@@ -28,7 +30,9 @@ REJECT_PREFIX="$REJECT_CHAIN: "
 # Play it safe from this point
 set -e
 
-# Clean-up all custom IPTABLES rules and chains
+#
+# Clean-up all custom IPTABLES rules and chains.
+#
 function cleanup {
     # Delete existing INPUT rules 
     iptables -n -L INPUT --line-numbers | awk "\$2==\"$CHECK_CHAIN\" { print \$1 }" | tac | xargs -r iptables -D INPUT
@@ -44,14 +48,38 @@ function cleanup {
     fi 
 }
 
-# Re-initialize CHECK and REJECT chains
+#
+# Re-initialize CHECK and REJECT chains.
+#
 function reinit {
     cleanup
     iptables -N "$CHECK_CHAIN"
+    iptables -A "$CHECK_CHAIN" -j LOG --log-prefix "$CHECK_PREFIX" --log-level 6
     iptables -A INPUT -p udp --dport $SERVER_PORT \! -f -m u32 --u32 "$CHECK_FILTER" -j "$CHECK_CHAIN" 
     iptables -N "$REJECT_CHAIN"
     iptables -A "$REJECT_CHAIN" -j LOG --log-prefix "$REJECT_PREFIX" --log-level 4
     iptables -A "$REJECT_CHAIN" -j DROP
+}
+
+#
+# Append new CHECK chain rule for the specified player.
+#
+function append {
+    PLAYER_NAME="$1"
+    if [ -z "$PLAYER_NAME" ]; then
+        echo "Missing player name."
+        exit 1
+    fi
+    NAME_HEX=$(xxd -pu <<< "$PLAYER_NAME")
+    # Standard check
+    NAME_LENGTH=$(printf "%02x\n" ${#PLAYER_NAME})
+    iptables -A "$CHECK_CHAIN" -j "$REJECT_CHAIN" \
+            -m string --hex-string "$NAME_LENGTH$NAME_HEX" --from 50 --to 51 --algo bm \
+            -m comment --comment "$PLAYER_NAME"
+    # Null-terminated check
+    iptables -A "$CHECK_CHAIN" -j "$REJECT_CHAIN" \
+            -m string --hex-string "$NAME_HEX"00 --from 51 --to 52 --algo bm \
+            -m comment --comment "$PLAYER_NAME"
 }
 
 case "${1:-''}" in
@@ -60,6 +88,9 @@ case "${1:-''}" in
         ;;
     reinit)
         reinit
+        ;;
+    append)
+        append "$2"
         ;;
     *)
         echo "Usage: $SELF cleanup|reinig"
